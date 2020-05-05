8a6b55f1a720b2bcaee107c9b4bfb9140f0c16a7
taron-jamal <61537513+taron-jamal@users.noreply.github.com>
2020-05-05 00:06:34 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+// Root Scope Example:
+
+var goatSoccerPlayer = "Franz Beckenbauer";
+
+function generationalSoccerPlayer1() {
+  // child scope
+  var goatSoccerPlayer = "Lionel Messi";
+  console.log(1, goatSoccerPlayer);
+}
+
+function generationalSoccerPlayer2() {
+  // child scope
+  var goatSoccerPlayer = "Diego Maradona";
+  console.log(2, goatSoccerPlayer);
+}
+
+function generationalSoccerPlayer3() {
+  // child scope
+  goatSoccerPlayer = "Pele";
+  console.log(3, goatSoccerPlayer);
+}
+
+console.log("window", goatSoccerPlayer);
+
+generationalSoccerPlayer1();
+generationalSoccerPlayer2();
+generationalSoccerPlayer3();
+
+console.log(goatSoccerPlayer);
